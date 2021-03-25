b332e3db056ad4410eb94ac1934670a2d634a2de
suryanto23 <sur23.id@gmail.com>
2021-03-25 20:38:16 +0700

    optimizing landing endpoint (add archon tags)

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,11 @@ router.get("/" , (req,res)=>{
        `
        <div style="margin-top:5%;">
         <h1 style="font-family: Arial; margin-left:40%;">Welcome to My Database</h1><br>
-        <p style="margin-left:42%;">Simply browse data by adding to URL : </p>
+        <p style="margin-left:42%;">Simply browse data by click or adding to URL : </p>
             <ul style="margin-left:43%;">
-                <li><p>User : /users</p></li>
+                <li><p><a href="https://suryanto-sequelize.herokuapp.com/users">User </a> : /users</p></li>
                 <li><p>User ID : /users/(id)</p></li>
-                <li><p>User Info : /users/info</p></li>
+                <li><p><a href="https://suryanto-sequelize.herokuapp.com/users/info">User Info</a> : /users/info</p></li>
                 <li><p>User Info ID : /users/info/(id)</p></li>
             </ul>
         </div>
